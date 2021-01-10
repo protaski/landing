6a34eecac0d20867a9a73dfd90dd3fc4b00bd6ab
Ronnie Myers <hi@ronniemyers.com>
2021-01-10 11:59:10 -0600

    firebase modules

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+
 const config = {
     apiKey: "AIzaSyBAJ3q09DlPGVll6OqhgGKKYMHx8Eme6GU",
     authDomain: "protaski-9.firebaseapp.com",
